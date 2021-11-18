62655c4777f15df930c5626c6cd7922ca25f7eb8
Cesar Lopez <clopez@logicasoft.io>
2021-11-17 21:06:10 -0600

    shopping list structure

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("onion", 2),
+    new Ingredient("lettuce", 1)
+  ]
 
   constructor() { }
 
